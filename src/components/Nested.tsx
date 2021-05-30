9fffb6c2b4bb56fdc700baa68c0eda7a520fbe2f
osequi <cs@osequi.com>
2021-05-30 15:41:18 +0300

    Adding examples

--- src/components/Nested.tsx
+++ src/components/Nested.tsx
@@ -35,6 +35,18 @@ export function Nested1a({ prop1, prop2 }: TNested = nestedFullDepth) {
   return <p>{`${text}: ${numbers}`}</p>; // undefined, undefined
 }
 
+/**
+
+// This doesn't works
+export function Nested1c({...defaultsDeep(prop1, prop2, nestedFullDepth)}: TNested) {
+  const text = prop1?.prop1a;
+  const numbers = prop2?.prop2a?.map((item) => item).join(",");
+  return <p>{`${text}: ${numbers}`}</p>; // undefined, undefined
+}
+
+
+ */
+
 export function Nested2(props: TNested) {
   const { prop1, prop2 } = props;
 
